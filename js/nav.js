aa29e12db324019b0bed9ddce10841ad175856fb
Rohit Dewani <rohit.jd23@gmail.com>
2022-05-09 20:09:30 +0530

    Add index page content

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.nav-toggle-btn');
+const svgBars = document.querySelector('.hamburger');
+const svgClose = document.querySelector('.close');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible',true);
+        svgBars.setAttribute('data-visible',false);
+        svgClose.setAttribute('data-visible',true);
+    }
+
+    if (visibility === "true"){
+        primaryNav.setAttribute('data-visible',false);
+        svgBars.setAttribute('data-visible',true);
+        svgClose.setAttribute('data-visible',false);
+    }
+
+});
